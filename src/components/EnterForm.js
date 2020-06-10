41dddd997f0c13369f07cbc7e3f99aa4ef123f9a
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-06-10 13:43:21 -0700

    idk

--- src/components/EnterForm.js
+++ src/components/EnterForm.js
@@ -10,15 +10,16 @@ const EnterForm = () => {
             <h1>Enter Room</h1>
             <input placeholder="Username" className="formInput" type="text" onChange={e => setUsername(e.target.value)} />
             <select placeholder="Select a Room" className="formInput" onChange={e => setRoom(e.target.value)}>
+                <option disabled="" default>Select a Room</option>
                 <option>Room 1</option>
                 <option>Room 2</option>
                 <option>Room 3</option>
             </select>
             <Link
-                onClick={e => (!username && !room) ? e.preventDefault() : null}
+                onClick={e => (!username || !room) ? e.preventDefault() : null}
                 to={`/chatroom?username=${username}&room=${room}`}
             >
-                <button className="formButton" type="submit">Enter</button>
+                <button className="formButton">Enter</button>
             </Link>
         </div>
     )
