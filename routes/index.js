0bb3044f94b47c69b3a561e44fc40ecaa0396386
Sarah <40401149+arghmatey@users.noreply.github.com>
2020-06-09 17:58:03 -0700

    idk

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.render('App');
+router.get('/', (req, res) => {
+    res.status(200);
 });
 
 module.exports = router;
